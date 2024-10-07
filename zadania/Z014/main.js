b366567a225585b5ef746ec959b6e3e7ee8bd9c7
2p02 <mychawa52@gmail.com>
2024-10-07 15:48:51 +0200

    zadanieeee

---
+++ zadania/Z014/main.js
@@ -0,0 +1,11 @@
+const iks = document.querySelectorAll("#iks");
+const wynik = document.querySelectorAll("#wynik");
+const btn = document.querySelectorAll("button");
+
+btn.addEventListener("click", function() {
+
+    let p1 = Math.floor(Math.iks );
+
+
+
+})
