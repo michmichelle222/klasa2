8c0784291a4359e751b38628aea7ecc3ae2c128f
2p02 <mychawa52@gmail.com>
2024-11-19 11:18:47 +0100

    zadanie 46

---
+++ zadania/Z045 - trojkaty/main.js
@@ -0,0 +1,31 @@
+const A = document.querySelector('#A')
+const B = document.querySelector('#B')
+const C = document.querySelector('#C')
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', function() {
+
+    let liczbaA = parseFloat(A.value);
+    let liczbaB = parseFloat(B.value);
+    let liczbaC = parseFloat(C.value);
+    let trojkat = 0;
+
+    if (liczbaA == liczbaB && liczbaC == liczbaA)
+        trojkat = "rownoboczny"
+    else if (liczbaA == liczbaB || liczbaB == liczbaC || liczbaC == liczbaA)
+        trojkat = "rownoramienny"
+    else if ((liczbaA * liczbaA + liczbaB * liczbaB ) == liczbaC * liczbaC)
+        trojkat = "prostokatny"
+    else
+        trojkat = "dowolny"
+    wynik.innerHTML =
+        `
+        A = ${liczbaA}<br>
+        B = ${liczbaB}<br>
+        C = ${liczbaC}<br>
+        Trójkąt jest ${trojkat}<br>
+        
+        
+        
+        `
+})
